8ac912a4e122d1a351c23029b57bee86e246a92a
coleWesterveld <93096602+coleWesterveld@users.noreply.github.com>
2025-04-09 23:37:39 -0400

    scaffolding out UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'widgets/audio_player_bottomsheet.dart';
+import 'widgets/live_listening_page.dart';
+import 'widgets/ondemand_listening_page.dart';
+import 'widgets/info_drawer.dart';
+// I have created separate widgets for each page, drawer, and the bottom audio bar.
+// these can be found to edit in widgets/
 
 void main() {
   runApp(const MyApp());
@@ -55,16 +61,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _displayBottomSheet = false;
 
-  void _incrementCounter() {
+  void _toggleBottomSheet() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      // setstate rebuilds with new changes - here the change is that we want to display the bottom sheet
+      // which is conditional upon this variable
+      _displayBottomSheet = !_displayBottomSheet;
     });
   }
 
@@ -76,50 +79,75 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+
+        // top appbar with logo and selecting pages
+        appBar: AppBar(
+
+          // hamburger button to open side drawer
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          ),
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Image.asset(
+            'assets/WORDMARK PNG.png',
+            height: 120,
+          ),
+          // page selection
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                child: Text("Live")
+              ),
+              Tab(
+                child: Text("On Demand")
+              ),
+            ],
+          ),
+        ),
+
+        body: TabBarView(
+          children: [
+
+            // I abstracted away this page into its own component, in widgets/live_listening_page.dart
+            LiveListeningPage(),
+            
+            OnDemandListeningPage()
           ],
         ),
+        
+        floatingActionButton: FloatingActionButton(
+          onPressed: _toggleBottomSheet,
+          
+          child: const Text("audio!"),
+          
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      
+        // we render the bottom sheet if _displayBottomSheet is true, else we render nothing (pass null argument)
+        // the AudioBottomSheet is a custom widget (like component), found in widgets/audio_player_bottomsheet.dart
+        bottomSheet: _displayBottomSheet ? AudioBottomSheet() : null,
+
+        drawer: Drawer(
+          // info drawer is a custom widget, found in widgets/
+          // the name is generic cuz i dont hactually know what we want here... ?
+          child: InfoDrawer(),
+        ),
+          
+        
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
