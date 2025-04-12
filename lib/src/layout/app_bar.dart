edc57b80a7d05b472d56156586869fbe6434d266
Julian Morley <julianm@lrjsolutions.ca>
2025-04-12 03:11:58 -0400

    build main appbar

---
+++ lib/src/layout/app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:ctwr_midtown_radio_app/src/layout/drawer.dart';
+
+import 'package:ctwr_midtown_radio_app/src/settings/view.dart';
+
+class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MainAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Theme.of(context).brightness == Brightness.dark
+      ? Image.asset('assets/images/logo_mic_black_on_white.png',
+        fit: BoxFit.cover,
+        height: 60,
+      )
+      : Image.asset('assets/images/logo_main.png',
+        fit: BoxFit.cover,
+        height: 120,
+      ),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.settings),
+          onPressed: () {
+            Navigator.pushNamed(context, SettingsPage.routeName);
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
