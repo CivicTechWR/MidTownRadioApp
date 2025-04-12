14cd6d94c9097212f310c591f7a5d81fd5da9627
Julian Morley <julianm@lrjsolutions.ca>
2025-04-12 03:12:09 -0400

    build main drawer

---
+++ lib/src/layout/drawer.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MainAppDrawer extends StatelessWidget {
+  const MainAppDrawer({
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Midtown Radio',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
+            onTap: () {
+              Navigator.pushNamed(context, '/');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () {
+              Navigator.pushNamed(context, '/settings');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
