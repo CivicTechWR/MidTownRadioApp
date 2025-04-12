b24c63051b5e6b90579e32dc2e39257957c33de8
Julian Morley <julianm@lrjsolutions.ca>
2025-04-12 03:11:31 -0400

    add ondemand page

---
+++ lib/src/on_demand/view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnDemandPage extends StatelessWidget {
+  const OnDemandPage({super.key});
+  static const routeName = '/on_demand';
+  final String title = 'On Demand';
+
+  @override
+  Widget build(BuildContext contect){
+    return Scaffold(
+      body: Center()
+    );
+  }
+}
