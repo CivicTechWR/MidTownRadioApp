0e2cc55d4b0f2d1830b5d856701255c03183728f
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-04-17 19:56:34 -0400

    Make OnDemandPage stateful

--- lib/src/on_demand/view.dart
+++ lib/src/on_demand/view.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-class OnDemandPage extends StatelessWidget {
+class OnDemandPage extends StatefulWidget {
   const OnDemandPage({super.key});
   static const routeName = '/on_demand';
   final String title = 'On Demand';
 
   @override
-  Widget build(BuildContext contect){
+  State<OnDemandPage> createState() => _ShowListState();
+}
+
+class _ShowListState extends State<OnDemandPage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center()
+      body: Center(
+        child: Text("Hello World!"),
+      ),
     );
   }
 }
