859f95d644385c3e5c5cc82a9cf6bd661df19df0
jChenvan <188939308+jChenvan@users.noreply.github.com>
2025-04-24 17:08:34 -0400

    Update live listening page to match color scheme
    The button and text on the live listening page
    now match the color scheme. The play button also
    has added functionality.

--- lib/src/listen_live/view.dart
+++ lib/src/listen_live/view.dart
@@ -17,22 +17,56 @@ class ListenLivePage extends StatelessWidget {
     final playerProvider = Provider.of<PlayerProvider>(context);
     return Center(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () => playerProvider.setStream('https://midtownradiokw.out.airtime.pro/midtownradiokw_a'), 
-            child: Icon(Icons.play_arrow, size: 100),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    playerProvider.setStream('https://midtownradiokw.out.airtime.pro/midtownradiokw_a');
+                    if (playerProvider.isPlaying) {
+                      playerProvider.pause();
+                      } else {
+                        playerProvider.play();
+                      }
+                    }, 
+                  style: ButtonStyle(
+                    fixedSize: WidgetStatePropertyAll(Size.fromRadius(100)),
+                    padding: WidgetStatePropertyAll(EdgeInsets.all(10))),
+                  child: Stack(alignment: AlignmentDirectional.center,children: [
+                    Container(
+                      width: 150, 
+                      height: 150, 
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xff005c5f),Color(0xff00989d),Color(0xff33cccc)],
+                          begin: Alignment.bottomCenter,
+                          end:Alignment.topCenter,
+                          ),
+                          shape:BoxShape.circle,
+                          ),),
+                    Icon(playerProvider.isPlaying ? Icons.pause:Icons.play_arrow,size: 100, color: Color.fromRGBO(217, 217, 216, 0.9),)
+                    ]),
+                ),
+                // Icon(Icons.radio, size: 100),
+                // PlayerWidget(),
+                // SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Text(
+                    'Live Listening',
+                    style: TextStyle(
+                      color: Color(0xfff05959),
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,),
+                  ),
+                ),
+                SizedBox(height: 10)
+              ],
+            ),
           ),
-          // Icon(Icons.radio, size: 100),
-          // PlayerWidget(),
-          SizedBox(height: 20),
-          Text(
-            'Listen Live!',
-            style: TextStyle(
-              fontSize: 34,
-              fontWeight: FontWeight.bold,),
-          ),
-          SizedBox(height: 10),
           PlayerWidget()
         ],
       )
