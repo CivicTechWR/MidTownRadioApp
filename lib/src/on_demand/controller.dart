ed7836863ea81bdf8a2ab808cd45068d6fb054f2
coleWesterveld <93096602+coleWesterveld@users.noreply.github.com>
2025-05-01 19:35:18 -0400

    dynamically fetch RSS feeds

--- lib/src/on_demand/controller.dart
+++ lib/src/on_demand/controller.dart
@@ -3,6 +3,10 @@ import 'package:dart_rss/dart_rss.dart';
 import 'dart:typed_data';
 import 'package:intl/intl.dart';
 
+// For debugprint
+import 'package:flutter/foundation.dart'; 
+
+
 class OnDemand {
   List<Episode> episodes = [];
   final Map<String, Uint8List> _imageCache = {};
@@ -15,9 +19,12 @@ class OnDemand {
     return onDemand;
   }
 
-  final List<String> streams = _Streams().getStreams();
-
+ 
   Future _fetchEpisodes() async {
+    // getStreams probably can be statically accessed, unless we had some future plans for it to expand significantly
+    // This also now needs to be awaited
+    final List<String> streams = await _Streams.getStreams();
+
     try {
       episodes.clear();
       for (var url in streams) {
@@ -125,14 +132,61 @@ class Episode {
       required this.episodeSortDate});
 }
 
+/*
+We want the app to fetch streams from an external source which can be updated easily, 
+so that when a podcast is added with a new RSS feed, the app updates.
+Here is how we are getting the RSS streams to parse:
+
+1. We have the hardcoded list of URLs for the podcasts that we already know of. 
+   This is a fallback -- it wont update automatically if a new podcast is added.
+
+2. We fetch a text file from an external source containing all current RSS URL's.
+   We parse it for the URL's, and any new ones not already in the hardcoded list are added to our streams
+
+We could maybe discuss persisting any newly found URL's onto the users device using Flutter shared_preferences or similar - though I don't see much benefit in this.
+We also might as well update the hardcoded list anytime we would have had to push an update anyways for another reason - but I think theres no point to making an update solely to update the list.
+
+Currently for testing, the file is on GitHub, but the plan is that it is on the existing Wix website for easy updating and more centralization.
+*/
 class _Streams {
-  final List<String> _streams = [
+
+  // This is the URL to the text file with updated RSS feeds
+  // currently on GitHub -- TODO: move to Wix
+  static const String feedsUrl = 
+    'https://raw.githubusercontent.com/USERNAME/REPO/main/feeds.txt';
+
+  // Hardcoded list - exists if for some reason new feeds can't fetch.
+  static const  List<String> _fallback = [
     'https://feeds.transistor.fm/midtown-radio',
     'https://feeds.transistor.fm/on-the-scene',
     'https://feeds.transistor.fm/makings-of-a-scene',
     'https://feeds.transistor.fm/midtown-conversations'
   ];
-  List<String> getStreams() {
-    return _streams;
+
+  // Get union of fallback and new loaded RSS URL's
+  static Future<List<String>> getStreams() async {
+    try {
+      final resp = await http.get(Uri.parse(feedsUrl));
+      if (resp.statusCode != 200) throw Exception();
+
+      // parses response body, separates out URLS and removes any extra whitespace
+      final List<String> remote = resp.body
+        .split('\n')
+        .map((l) => l.trim())
+        .where((l) => l.isNotEmpty)
+        .toList();
+
+      // merge remote + fallback, preserve remote order
+      final merged = <String>[];
+      merged.addAll(remote);
+      merged.addAll(_fallback.where((u) => !merged.contains(u)));
+
+      return merged;
+
+    } catch (_) {
+      debugPrint('StreamsRepository: fallback to hard-coded streams');
+      return _fallback;
+    }
   }
 }
+
